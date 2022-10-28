b4bf646554f65ed0cb9d657ea8fdd7d85f437d6c
Filipe Anjos <110917838+Anjosfilipe@users.noreply.github.com>
2022-10-28 13:23:57 -0300

    criação e atualização da startup

--- Flight/Startup.cs
+++ Flight/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
--- Passenger/Startup.cs
+++ Passenger/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using Passangers.Utils;
 using Passengers.Services;
